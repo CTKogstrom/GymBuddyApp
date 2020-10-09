c7e35e3cb965eb3d5b0e6f423573da77dcd23a8d
Ryan Ahmed <ra700@hotmail.com>
2020-10-09 03:57:36 -0400

    set up navigation and pages

--- gymBuddyApp/urls.py
+++ gymBuddyApp/urls.py
@@ -17,15 +17,19 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+#we do not need to import views from pages since pages already has a urls.py file
 
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('home/', include('pages.urls')),
-    path('home/register/', user_views.register, name='register'),
-    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
+    path('register/', user_views.register, name='register'),
+    path('weight/', user_views.weight, name='weight'),
+    path('macros/', user_views.macros, name='macros'),
+    path('exercises/', user_views.exercises, name='exercises'),
+    path('meals/', user_views.meals, name='meals'),
+    path('login/', user_views.login, name='login')
 ]

bbebe56c8a9bc83d7ea81422136e621035ff870f
Ryan Ahmed <ra700@hotmail.com>
2020-10-14 01:50:28 -0400

    trying if statement for if logged in

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 
 def home(request):
+    loggedIn = False
+    if request.user.is_authenticated:
+        loggedIn = True
     return render(request, 'pages/home.html', {})
 
 

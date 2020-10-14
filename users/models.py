fd7480d963de76ef526d0de945353aa638603481
Carl Kogstrom <carl.kogstrom@gmail.com>
2020-10-14 09:31:36 -0400

    Added weight records and lift records associated with a user

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from activityLibrary.models import Exercise
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,7 +11,23 @@ class Profile(models.Model):
     daily_protein = models.IntegerField()
     goal_weight_change = models.IntegerField()
     activity_level = models.DecimalField(max_digits=2, decimal_places=1)
-    current_weight = models.IntegerField()
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class WeightRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lbs = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username}: {self.lbs} on {self.date}"
+
+class LiftRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField(blank=True, null=True)
+    reps_per_set = models.IntegerField(blank=True, null=True)
+    date = models.DateField()
+
+

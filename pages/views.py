bed3d8928ecf0e2432aecbd6a5a54146cdabbbd1
Ryan Ahmed <ra700@hotmail.com>
2020-10-14 02:10:45 -0400

    added logout link

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 
 def home(request):
-    loggedIn = False
+    context = {
+        'loggedIn': False
+    }
     if request.user.is_authenticated:
-        loggedIn = True
-    return render(request, 'pages/home.html', {})
+        context['loggedIn'] = True
+   
+    return render(request, 'pages/home.html', context)
 
 
 
